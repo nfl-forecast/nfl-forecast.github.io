5f25cab75e9b8a8b545fb01aeee62f80583d6b46
Adamnathaniel <adamngoldstein@gmail.com>
2018-12-24 17:18:55 -0500

    Reformats the getting from API program

--- Workspace/Data/src/GetOverallTeamStandings.java
+++ Workspace/Data/src/GetOverallTeamStandings.java
@@ -1,71 +1,44 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Writer;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Scanner;
 
-
 public class GetOverallTeamStandings {
 
-	public static String get()
-	{
-		 try {
-	        	
-	            Scanner scan =new Scanner(new File("APIKey")); 
-	            String token = scan.next(); //reads in the api key
-	            //String encoding = token;
-	            String encoding = Base64.getEncoder().encodeToString( (token).getBytes());
-	            URL url = new URL ("https://api.mysportsfeeds.com/v1.0/pull/nfl/latest/overall_team_standings.json");
-	            
-	            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-	            connection.setRequestMethod("GET");
-	            connection.setDoOutput(true);
-	            connection.setRequestProperty("Authorization", "Basic " + encoding);
-	            InputStream content = (InputStream)connection.getInputStream();
-	            BufferedReader in = 
-	                new BufferedReader(new InputStreamReader(content));
-	            String line;
-	            while ((line = in.readLine()) != null) {
-	            	return line;
-	            }
-	        } catch(Exception e) {
-	            e.printStackTrace();
-	        }
-		 return null;
+	public static String get() {
+		return get("latest");
 	}
-    public static void main(String[] args) {
 
-        try {
-        	
-            Scanner scan =new Scanner(new File("APIKey")); 
-            String token = scan.next(); //reads in the api key
-            //String encoding = token;
-            String encoding = Base64.getEncoder().encodeToString( (token).getBytes());
-            URL url = new URL ("https://api.mysportsfeeds.com/v1.0/pull/nfl/latest/overall_team_standings.json");
-            
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setDoOutput(true);
-            connection.setRequestProperty("Authorization", "Basic " + encoding);
-            InputStream content = (InputStream)connection.getInputStream();
-            BufferedReader in = 
-                new BufferedReader(new InputStreamReader(content));
-            String line;
-            while ((line = in.readLine()) != null) {
-            	System.out.println(line);
-                BufferedWriter f = new BufferedWriter(new FileWriter("OvrStandings.json"));
-                f.write(line);
-            }
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
-  
+	public static void main(String[] args) {
+		get();
+	}
+	public static String get(String year) {
+		try {
+
+			Scanner scan = new Scanner(new File("APIKey"));
+			String token = scan.next(); // reads in the api key
+			// String encoding = token;
+			String encoding = Base64.getEncoder().encodeToString((token).getBytes());
+			URL url = new URL("https://api.mysportsfeeds.com/v1.0/pull/nfl/" + year + "/overall_team_standings.json");
+
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setDoOutput(true);
+			connection.setRequestProperty("Authorization", "Basic " + encoding);
+			InputStream content = (InputStream) connection.getInputStream();
+			BufferedReader in = new BufferedReader(new InputStreamReader(content));
+			String line;
+			while ((line = in.readLine()) != null) {
+				return line;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
 }
