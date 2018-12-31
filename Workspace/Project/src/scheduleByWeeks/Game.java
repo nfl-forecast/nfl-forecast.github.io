ce701656fead6aa0a2cc456768d7d61f0fafb6fb
Adamnathaniel <adamngoldstein@gmail.com>
2018-12-31 15:57:27 -0500

    reformats and fixes games won calculation

--- Workspace/Project/src/scheduleByWeeks/Game.java
+++ Workspace/Project/src/scheduleByWeeks/Game.java
@@ -2,63 +2,49 @@ package scheduleByWeeks;
 
 import teamStructure.Team;
 
-public class Game
-{
+public class Game {
 	Team awayTeam, homeTeam;
 	double awayPCT, homePCT;
-	
-	public Game(Team away, Team home)
-	{
+
+	public Game(Team away, Team home) {
 		awayTeam = away;
 		homeTeam = home;
 		calculate();
 	}
+
 	/**
 	 * @return The home team
 	 */
-	public Team getHomeTeam()
-	{
+	public Team getHomeTeam() {
 		return homeTeam;
 	}
+
 	/**
 	 * 
 	 * @return The home team
 	 */
-	public Team getAwayTeam()
-	{
+	public Team getAwayTeam() {
 		return awayTeam;
 	}
+
 	/**
 	 * Makes percentages to represent each teams chances to win the game
 	 */
-	public void calculate()
-	{
-		//TODO: Set the formula to calculate percent chance to win
+	public void calculate() {
+		// TODO: Set the formula to calculate percent chance to win
 		setRecords();
 	}
+
 	/**
 	 * Changes the teams records to show who was predicted to win
 	 */
-	private void setRecords()
-	{
-		if(awayTeam.getFPI() > homeTeam.getFPI())
-		{
-			awayTeam.addWin(false, homeTeam, awayPCT);
-			homeTeam.addLoss(true, awayTeam, homePCT);
-		}
-		else if(awayTeam.getFPI() < homeTeam.getFPI())
-		{
-			awayTeam.addLoss(false, homeTeam, awayPCT);
-			homeTeam.addWin(true, awayTeam, homePCT);
-		}
-		else
-		{
-			awayTeam.addtie(false, homeTeam, awayPCT);
-			homeTeam.addtie(true, awayTeam, homePCT);
-		}
+	private void setRecords() {
+		awayTeam.addResult(false, homeTeam, awayPCT);
+		homeTeam.addResult(true, awayTeam, homePCT);
+
 	}
-	public String toString()
-	{
+
+	public String toString() {
 		return awayTeam.toString() + " @ " + homeTeam.toString();
 	}
 }
