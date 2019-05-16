6375beded7c943f7cbcc58b3ae11ed328f94a367
Max <maxnbf@gmail.com>
2019-05-16 08:37:37 -0400

    starts fixing weekbuttons

--- weekButtons.js
+++ weekButtons.js
@@ -11,4 +11,12 @@ angular.module('nflforecast').component('weekButtons', {
 function WBController()
 {
   this.selected = 0;
+
+  this.firstLine;
+  this.secondLine;
+
+  this.$onInit = function(){
+    this.firstLine = data.season.weeks.slice(0, 9);
+    this.secondLine = data.season.weeks.slice(9);
+}
 }
